e5a5c9b6d801fa006c5b27ae2e16168985f7ed4d
jimmy_sidney <jimmy_sidney@hotmail.es>
2016-05-03 08:29:47 -0600

    fixed

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 SHA1=$1
-EB_BUCKET=s3-demo-bucket-bit
+BUCKET=$2
 
 
 # Deploy image to Docker Hub
@@ -12,7 +12,7 @@ docker push jimmyrosa/sunodejs:$SHA1
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name bit-portal --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE --region us-east-2
 
  # Update Elastic Beanstalk environment to new version
